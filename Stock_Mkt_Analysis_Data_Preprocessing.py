4311fb9729e52fee8872e35328d0db54b2be19a2
Subeer Kumar <35768470+subeerk@users.noreply.github.com>
2018-11-14 00:02:50 +0530

    code changes for VIX and currency rates

--- Stock_Mkt_Analysis_Data_Preprocessing.py
+++ Stock_Mkt_Analysis_Data_Preprocessing.py
@@ -9,6 +9,10 @@
 # * forex rate (US vs INR)
 # * VIX rates
 
+START_DATE = "2018,1,1"
+END_DATE = "2018,9,1"
+
+
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
@@ -17,12 +21,36 @@ engine = create_engine('sqlite://', echo=False)
 from datetime import date
 from nsepy import get_history
 
-# read a configuration file that would have the list of the stocks to get data for
-# this configuration file would also have the fields START_DATE and END_DATE marking
-# the date range for which the data has to be collected
 
-sbin = get_history(symbol='PNB', start=date(2018,1,1),end=date(2018,9,1))
-
+pnb = get_history(symbol='PNB', start=date(2018,1,1),end=date(2018,9,1))
 sbin = get_history(symbol='SBIN', start=date(2018,1,1),end=date(2018,9,1))
+
+sbin.head()
+
 sbin.to_sql('sbin',con=engine)
+pnb.to_sql('pnb',con=engine)
+
+'''
 engine.execute('SELECT * from sbin').fetchall()
+
+[('2018-01-01', 'PNB', 'EQ', 171.4, 172.95, 173.4, 168.9, 169.7, 169.75, 171.31, 7869149, 134803219700000.0, 35893, 1423821, 0.1809),
+ ('2018-01-02', 'PNB', 'EQ', 169.75, 170.5, 170.9, 165.4, 166.15, 166.4, 167.26, 13729132, 229638327805000.03, 54266, 3567217, 0.25980000000000003),
+
+'''
+vix = get_history(symbol='INDIAVIX', start=date(2018,1,1),end=date(2018,1,1),index=True)
+vix.to_sql('vix',con=engine)
+
+'''
+vix.head()
+
+            Open	High	Low	Close	Previous	Change	%Change
+Date	     						
+            NaN 	NaN 	NaN 	NaN 	NaN 	NaN 	NaN
+2018-01-01	12.67	13.6125	12.545	13.3525	12.67	0.68	0.0539
+'''
+from nsepy import get_rbi_ref_history
+rbi_ref = get_rbi_ref_history(date(2015,1,1), date(2015,1,10))
+rbi_ref['1 USD'].to_sql('curr_rates', con = engine)
+engine.execute('SELECT * from curr_rates').fetchall()
+
+engine.table_names()
