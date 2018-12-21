d981b67effdbb7178010f290f0e565c86392f89f
Subeer Kumar <35768470+subeerk@users.noreply.github.com>
2018-12-21 10:50:14 +0530

    Update get_bhavdata.py
    added code changes for connecting to the MySQL installed on localhost

--- get_bhavdata.py
+++ get_bhavdata.py
@@ -23,6 +23,7 @@ import sys
 from urllib.request import urlretrieve
 from pathlib import Path
 from datetime import datetime, timedelta
+import mysql.connector
 
 # see how many days, the records are needed for
 if (int(sys.argv[1]) < 1):
@@ -52,3 +53,19 @@ for i in range(int(sys.argv[1])):
         print("Could not download for the date ",working_day)
     #file_name = os.path.join(f"cm{day}{month}{year}bhav.csv")
     #dataframe = pd.read_csv(file_name)
+
+# Connecting to the MySQL database installed on the local machine
+# the downloaded .CSV files would be added to the database here
+# As a pre-requisite, here are the setup done on the MySQL server
+#
+# Database created is - database1
+# the installation is done with default username - root
+# the password to the mysql server is hardcoded in the code as of now
+# the password is shown as XXXXXXXXXX for security purpose
+
+try:
+   connection = mysql.connector.connect(host='localhost', database='database1', user='root', password='XXXXXXXXXX')
+   db_Info = connection.get_server_info()
+   print("Conneceted to the MySQL server database with version ",db_Info)
+except:
+   print("Could not connect to the MySQL server...")
